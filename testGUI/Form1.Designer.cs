01419ff958dba8fc9e81ed062972ac306818556a
khiem <khiemnv@hotmail.com>
2020-10-15 01:42:11 +0900

    test GUI: javascript -> gecko

---
+++ testGUI/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace testGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup1", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup2", System.Windows.Forms.HorizontalAlignment.Center);
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "1"),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "one", System.Drawing.SystemColors.WindowText, System.Drawing.SystemColors.HotTrack, new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "tow", System.Drawing.SystemColors.WindowText, System.Drawing.SystemColors.MenuHighlight, new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "three", System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128))))), System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128))))), new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128))))}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "2"),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "111", System.Drawing.SystemColors.HotTrack, System.Drawing.SystemColors.Window, new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "222", System.Drawing.Color.Red, System.Drawing.SystemColors.Window, new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128))))}, -1);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.content = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.para = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.diff = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.detail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.content,
+            this.title,
+            this.para,
+            this.pos,
+            this.diff,
+            this.detail});
+            listViewGroup1.Header = "ListViewGroup1";
+            listViewGroup1.Name = "listViewGroup2";
+            listViewGroup2.Header = "ListViewGroup2";
+            listViewGroup2.HeaderAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            listViewGroup2.Name = "listViewGroup2";
+            listViewGroup2.Tag = "1";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2});
+            this.listView1.HideSelection = false;
+            listViewItem1.Group = listViewGroup1;
+            listViewItem2.Group = listViewGroup2;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(400, 191);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Visible = false;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(12, 230);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(401, 163);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            this.listView2.Visible = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(418, 220);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(192, 182);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader content;
+        private System.Windows.Forms.ColumnHeader title;
+        private System.Windows.Forms.ColumnHeader para;
+        private System.Windows.Forms.ColumnHeader pos;
+        private System.Windows.Forms.ColumnHeader diff;
+        private System.Windows.Forms.ColumnHeader detail;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewLinkColumn Column2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ testGUI/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace testGUI
+{
+    public partial class Form1 : Form
+    {
+        SearchPanel m_srchPanel;
+        Gecko.GeckoWebBrowser m_wb;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //m_srchPanel = new SearchPanel(cnnStr, false);
+            //m_srchPanel.OnSelectTitle += (s, e) =>
+            //{
+            //    Debug.WriteLine("titleId {0}", e);
+            //};
+            //this.Controls.Add(m_srchPanel.m_tblLayout);
+            //this.AcceptButton = m_srchPanel.m_acceptBtn;
+
+            m_wb = new Gecko.GeckoWebBrowser();
+            m_wb.Dock = DockStyle.Fill;
+            //m_wb.AddMessageEventListener("func1", onFunc1);
+            //wb.DomClick += Wb_DomClick;
+            //wb.LoadHtml("<html><body></body></html>", "http://blank");
+            this.Controls.Add(m_wb);
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+#if false
+        //LoadHtml() throw assert
+            m_wb.LoadHtml(@"<!DOCTYPE html>
+             <html><head>
+             <meta http-equiv=""Content-Type"" content=""text/html; charset=UTF-8"">
+             <script type=""text/javascript"">
+                function fireEvent(name, data)
+                {
+                   var event = new MessageEvent(name,{'view': window, 'bubbles': false, 'cancelable': false, 'data': data});
+                   document.dispatchEvent(event);
+                }
+             </script>
+             </head>
+             <body>fdsfsda
+                <input type=""button"" onclick=""fireEvent('myFunction', 'some data');"" value=""SHOW DATA"" />
+             </body></html>"
+            );
+
+            m_wb.AddMessageEventListener("myFunction", ((string s) => MessageBox.Show(s)));
+#endif
+
+            m_wb.AddMessageEventListener("func1", onFunc1);
+            m_wb.Navigate(@"C:\temp\until\KinhNikaya\testGUI\click.html");
+        }
+
+        private void onFunc1(string p)
+        {
+            MessageBox.Show(p);
+        }
+
+        private void Wb_DomClick(object sender, Gecko.DomMouseEventArgs e)
+        {
+            var msg = e.Button.ToString();
+            var tar = e.Target.ToString();
+            var el = e.Target.CastToGeckoElement();
+            var tag = el.TagName;
+            var type = el.GetAttribute("type");
+            var id = el.GetAttribute("id");
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var lvi = new ListViewItem();
+            //lvi.Text = "1";
+            //listView2.Items.Add(lvi);
+            //m_wb.AddMessageEventListener("func1", onFunc1);
+            //m_wb.Navigate(@"C:\temp\until\KinhNikaya\testGUI\click.html");
+            
+        }
+
+        string cnnStr
+        {
+            get
+            {
+                string srcDb = @"..\..\..\..\truongbo.accdb";
+                var cnnStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=<db>;";
+                return cnnStr.Replace("<db>", srcDb);
+            }
+        }
+
+        void testHeap()
+        {
+            int[] arr = new int[10];
+            var rnd = new Random(101);
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+            Func<int, int, int> cmp = (x, y) => x - y;
+            var h = new MyHeap<int>(arr, cmp);
+            for (int i = 0; i < 10; i++)
+            {
+                var min = h.PopMin();
+            }
+        }
+    }
+}
